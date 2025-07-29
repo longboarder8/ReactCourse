f4da9acdca6ba619a2cb32a315e9c92f4e5143b1
Brian Hamada <longboarder8@github.com>
2025-07-28 21:50:07 -0700

    Add Week3 project with production build

---
+++ .history/src/CustomNavBar_20250728201645.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Navbar, Nav, Container } from 'react-bootstrap';
+import './NavBarFix.css';
+
+function CustomNavBar() {
+  return (
+    <Navbar bg="light" expand="lg">
+      <Container>
+        <Navbar.Brand href="#">Week 3 Project</Navbar.Brand>
+        <Nav className="mx-auto">
+          <Nav.Link href="#">Link 1</Nav.Link>
+          <Nav.Link href="#">Link 2</Nav.Link>
+          <Nav.Link href="#">Link 3</Nav.Link>
+        </Nav>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default CustomNavBar;
